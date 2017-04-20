6b37ada9563d2cf33a7c72e7cd975465d03ec950
Jeffrey Chang <jeffreycj.chang@gmail.com>
2017-04-20 16:58:21 -0400

    Added initial project files

---
+++ src/routes/Calculator/components/Calculator.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+export const Calculator = (props) => (
+  <div style={{ margin: '0 auto' }} >
+    <h1>Giant Calculator here</h1>
+  </div>
+)
+
+Calculator.propTypes = {
+
+}
+
+export default Calculator
---
+++ src/routes/Home/components/HomeView.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import DuckImage from '../assets/Duck.jpg'
+import './HomeView.scss'
+
+export const HomeView = () => (
+  <div>
+    home container
+  </div>
+)
+
+export default HomeView
